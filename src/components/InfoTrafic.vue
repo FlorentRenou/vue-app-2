000cbac1d04541d9ac143628bc6a12af91ed5f5f
RENOU Florent <florent.renou01@gmail.fr>
2021-03-09 15:06:58 +0100

    Tentative

--- src/components/InfoTrafic.vue
+++ src/components/InfoTrafic.vue
@@ -9,6 +9,9 @@
                 </div>
             </div>
             <br>
+            <div class="bar">
+                <input type="text" v-model="searchString" placeholder="Entrer votre Arrêt" />
+            </div>
             <br>
             <div class="row" v-for="record in infos.records" >
                 <div class="col-md-12">
@@ -38,13 +41,17 @@
 </template>
 
 <script>
-    const apiURL = 'https://data.nantesmetropole.fr/api/records/1.0/search/?dataset=244400404_info-trafic-tan-temps-reel&q=&sort=date_debut'
+    const apiURL_1 = 'https://data.nantesmetropole.fr/api/records/1.0/search/?dataset=244400404_info-trafic-tan-temps-reel&q=&sort=date_debut'
+    const apiURL_2 = 'https://data.nantesmetropole.fr/api/records/1.0/search/?dataset=244400404_tan-arrets&q=&rows=10&refine.stop_id='
 
     export default {
         name: 'InfoTrafic',
         data() {
             return {
-            infos: []
+            infos: [],
+            arrets_1: [],
+            arrets_2: [],
+            searchString: ""
             }
         },
         created: function () {
@@ -53,8 +60,17 @@
         methods: {
             fetchData: async function () {
                 try {
-                    const response = await this.axios.get(apiURL)
-                    this.infos = response.data
+
+                    const response = await this.axios.get(apiURL_1);
+                    this.infos = response.data;
+                    infos.forEach(data => arrets_1 = data.fields.listes_arrets);
+                    console.log(arrets_1);
+
+                    //arrets_1.forEach(data => {
+                    //    const reponse2 = await this.axios.get(apiURL_2 + data);
+                    //    arrets_2 = arrets_2 + reponse2.data;
+                    //}
+
                 } catch (error) {
                     console.log(error);
                 }
